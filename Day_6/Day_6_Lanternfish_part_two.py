4b0bc94aa3f3d072ccd7616b61dc74cc79d1819f
MarcelSuleiman <91497205+MarcelSuleiman@users.noreply.github.com>
2021-12-07 17:19:23 +0100

    pasted link to task
    and removed not necessary blank lines

--- Day_6/Day_6_Lanternfish_part_two.py
+++ Day_6/Day_6_Lanternfish_part_two.py
@@ -1,29 +1,23 @@
+# link to task: https://adventofcode.com/2021/day/6
+
 # Better solution
 
 #baseFishesDays = [3,4,3,1,2]
 baseFishesDays = [5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2]
 
-
-
-
-						#    0, 1, 2, 3, 4, 5, 6 ,7 ,8
+		# indexes    0, 1, 2, 3, 4, 5, 6 ,7 ,8
 allFischesByDays_TEMPLATE = [0, 1, 1, 2, 1, 0, 0, 0, 0]
-allFischesByDays =			[0, 0, 0, 0, 0, 0, 0, 0, 0]
+allFischesByDays =	    [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for day in range(0,9):
 	sumOfFisches = baseFishesDays.count(day)
 	allFischesByDays[day] += sumOfFisches
 
-
-#print(allFischesByDays)
-
 for day in range(0, 256):
-
 	resetedAndNewBornFisches = [0,0]
 	shiftedFisches = [0,0,0,0,0,0,0,0,0]
 
 	for fishIndex in range(len(allFischesByDays)):
-
 		if fishIndex == 0:
 			resetedAndNewBornFisches[0] = allFischesByDays[0]
 			resetedAndNewBornFisches[1] = allFischesByDays[0]
@@ -40,14 +34,9 @@ for day in range(0, 256):
 			allFischesByDays[8] = resetedAndNewBornFisches[1] # 8d
 	
 	# Only for visual inspection
-
 	#print(f'		 0, 1, 2, 3, 4, 5, 6, 7, 8 ')
 	#print(f'Day: {day}. {allFischesByDays}')
 	#print()
 
-
 #print(allFischesByDays)
 print(sum(allFischesByDays))
-
-
-
