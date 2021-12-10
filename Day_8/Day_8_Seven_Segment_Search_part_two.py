c588908bdc6c61eb73b0e21a8b7e408a629bce96
MarcelSuleiman <91497205+MarcelSuleiman@users.noreply.github.com>
2021-12-10 02:02:52 +0100

    Update Day_8_Seven_Segment_Search_part_two.py

--- Day_8/Day_8_Seven_Segment_Search_part_two.py
+++ Day_8/Day_8_Seven_Segment_Search_part_two.py
@@ -1,22 +1,24 @@
+
+# link to task: https://adventofcode.com/2021/day/8
+
 h = '''
   0:      1:      2:      3:      4:
- aaaa            aaaa    aaaa        
-b    c       c       c       c  b    c
-b    c       c       c       c  b    c
-                 dddd    dddd    dddd
-e    f       f  e            f       f
-e    f       f  e            f       f
- gggg            gggg    gggg        
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
 
   5:      6:      7:      8:      9:
  aaaa    aaaa    aaaa    aaaa    aaaa
-b       b            c  b    c  b    c
-b       b            c  b    c  b    c
- dddd    dddd            dddd    dddd
-     f  e    f       f  e    f       f
-     f  e    f       f  e    f       f
- gggg    gggg            gggg    gggg
-
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
 '''
 
 inputFile = open('input.txt', 'r')
